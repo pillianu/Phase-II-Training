70fa4d600f6408625cb4fb9c4e7c1d1b8b3ac5d8
Pilli Anusha <142586280+pillianu@users.noreply.github.com>
2024-05-24 20:51:33 +0530

    Add files via upload

---
+++ insertionsort.py
@@ -0,0 +1,16 @@
+def performInsertionSort(nums):
+    n=len(nums)
+    for firstEleIndex in range(1,n):
+        temp=nums[firstEleIndex]
+        prevIndex=firstEleIndex-1
+        while prevIndex>=0:
+            if nums[prevIndex]>temp:
+                nums[prevIndex+1]=nums[prevIndex]
+            else:
+                break
+            prevIndex-=1
+        nums[prevIndex+1]=temp
+nums=[9,8,7,6,5,4,3,2,10]
+print("Before Sorting",nums)
+performInsertionSort(nums)
+print("After Sorting",nums)
